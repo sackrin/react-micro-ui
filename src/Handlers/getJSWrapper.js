a74246bb1db2e6139d9c66ffe498cec25cb29884
Hilary Osborne <h.t.osborne@gmail.com>
2020-03-22 16:50:21 +1000

    update JS wrapper to actually work

--- src/Handlers/getJSWrapper.js
+++ src/Handlers/getJSWrapper.js
@@ -1,18 +1,22 @@
 const getJSWrapper = (name, config, props, el) => ('<div data-microui-library="' + config.name + '" data-microui-component="' + name + '">' +
   '' + el + '' +
+  '</div>' +
   '<script type="application/javascript">' +
-  ' if (!!document.getElementById(`' + config.name + 'Library`)) { ' +
-  '   var tag = document.createElement(\'script\'); ' +
-  '   tag.id = `' + config.name + 'Library`; ' +
-  '   tag.src = `' + config.api.url + '/bootstrap.js`; ' +
-  '   document.body.appendChild(tag); ' +
-  ' } ' +
-  ' (function(n, c, p, t) { ' +
-  '   var w = window, m = function(e){ if (e.detail.library === n)' +
-  '     w[n].default.hydrateComponent(document.querySelector(\`div[data-microui-component="' + name + '"] div[data-reactroot]\`),c,p,t); };' +
-  '     if (w[n]) { m(); } else { w.addEventListener(\'microUILoaded\', m); }' +
-  '   })(\'' + config.name + '\', \'' + name + '\', ' + JSON.stringify(props) + ', {});' +
-  '</script>' +
-  '</div>');
+  ' (function(n, c, p, t) {' +
+  '  var w = window, m = function(e){' +
+  '   if (e.detail.name === n && w[n] && w[n].Hydrate) {' +
+  '    w[n].Hydrate(document.querySelector(`div[data-microui-component="' + name + '"] div[data-reactroot]`),c,p,t);' +
+  '   };' +
+  '  };' +
+  '  if (w[\'exampleMicroUI\']) { m(); } else { w.addEventListener(\'microUILoaded\', m); }' +
+  '  var d = document.getElementById(`' + config.name + 'Library`);' +
+  '  if (d === null || d.length === 0) {' +
+  '   var tag = document.createElement(\'script\');' +
+  '   tag.id = `' + config.name + 'Library`;' +
+  '   tag.src = `' + config.api.url + '/bootstrap.js`;' +
+  '   document.body.appendChild(tag);' +
+  '  }' +
+  ' })(\'' + config.name + '\', \'' + name + '\', ' + JSON.stringify(props) + ', {});' +
+  '</script>');
 
 export default getJSWrapper;
