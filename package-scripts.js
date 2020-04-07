63c954dda666b2a59aa5649d0fd3aa104f1abaeb
Hilary Osborne <h.t.osborne@gmail.com>
2020-04-07 12:06:57 +1000

    now generates type definitions at build

--- package-scripts.js
+++ package-scripts.js
@@ -10,9 +10,10 @@ module.exports = {
     },
     build: {
       description: 'Builds the assets',
-      default: series('npx nps clean', 'npx nps build.lib', 'npx nps build.assets'),
+      default: series('npx nps clean', 'npx nps build.lib', 'npx nps build.types', 'npx nps build.assets'),
       watch: 'nodemon --watch src --exec npx nps build',
       lib: series(mkdirp('lib'), `npx babel src --extensions '.ts,.tsx,.js' --config-file ./babel.lib.config.json --out-dir ./lib`),
+      types: 'npx tsc --declaration --outDir lib/ --emitDeclarationOnly --declarationMap',
       assets: series(mkdirp('lib/bootstrap'), `npx webpack --config ./webpack.config.js`),
     },
     clean: {
